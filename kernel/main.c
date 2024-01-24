6c6c1ce0c805d63212ee5414137383092f0fd6f7
hjl <2690938144@qq.com>
2024-01-24 11:12:15 +0800

    style: 格式化代码

--- kernel/main.c
+++ kernel/main.c
@@ -4,14 +4,13 @@
 extern void sbss();
 extern void ebss();
 
-void main()
-{
+void main() {
     /* 初始化 .bss 段 */
     uint64 *bss_start_init = (uint64 *)sbss, *bss_end_init = (uint64 *)ebss;
-    for (volatile uint64 *bss_mem = bss_start_init; bss_mem < bss_end_init; ++bss_mem)
-    {
+    for (volatile uint64 *bss_mem = bss_start_init; bss_mem < bss_end_init;
+         ++bss_mem) {
         *bss_mem = 0;
     }
-    printf("Hello from Moonix!\n");
+    printf("Hello World!\n");
     shutdown();
 }
